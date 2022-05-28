4d5451e4a5c6e218343ba1ba61dfe355e36fc7d2
Mariusz Banaś <mb0986@wp.pl>
2022-05-28 17:41:21 +0200

    Fix SeatChooser.js

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -8,7 +8,7 @@ class SeatChooser extends React.Component {
   
   componentDidMount() {
     const { loadSeats, loadSeatsData } = this.props;
-    this.socket = io(process.env.PORT || 'http://localhost:8000');
+    this.socket = io((process.env.NODE_ENV === 'production') ? '/' : 'http://localhost:8000');
     loadSeats();
 
     this.socket.on('seatsUpdated', (seats) => {
