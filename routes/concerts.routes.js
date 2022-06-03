15232f268a07affd029b13da871667a436c9153a
Mariusz Banaś <mb0986@wp.pl>
2022-06-03 16:17:25 +0200

    Task 29.5

--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -1,48 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
-const { v4: uuidv4 } = require('uuid');
+
+const ConcertController = require('../controllers/concerts.controller');
 
 // get all concerts
-router.route('/concerts').get((req, res) => {
-  res.json(db.concerts);
-});
 
-router.route('/concerts').get((req, res) => {
-  res.json(db.concerts);
-});
-  
-router.route('/concerts/:id').get((req, res) => {
-  res.json(db.concerts.find(i => i.id === parseInt(req.params.id)));
-});
-  
-router.route('/concerts').post((req, res) => {
-  const id = uuidv4();
-  const {performer, genre, price, day, image} = req.body;
-  
-  db.concerts.push({id: id, performer: performer, genre: genre, price: price, day: day, image: image});
-  res.json({message: 'OK'});
-});
-  
-router.route('/concerts/:id').delete((req, res) => {
-  const item = db.concerts.find(i => (i.id === parseInt(req.params.id)));
-  const itemIndex = db.concerts.indexOf(item);
-  
-  db.concerts.splice(itemIndex, 1);
-  res.json({message: 'OK'});
-});
-  
-router.route('/concerts/:id').put((req, res) => {
-  const item = db.concerts.find(i => (i.id === parseInt(req.params.id)));
-  const { performer, genre, price, day, image } = req.body;
-  
-  item.performer = performer;
-  item.genre = genre;
-  item.price = price;
-  item.day = day;
-  item.image = image;
-  
-  res.json({message: 'OK'});
-});
+router.get('/concerts', ConcertController.getAll);
+
+router.get('/concerts/:id', ConcertController.getById);
+
+router.post('/concerts', ConcertController.post);
+
+router.delete('/concerts/:id', ConcertController.delete);
+
+router.put('/concerts/:id', ConcertController.put);
 
 module.exports = router;
--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -1,54 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
-const { v4: uuidv4 } = require('uuid');
+
+const SeatController = require('../controllers/seats.controller');
 
 // get all seats
-router.route('/seats').get((req, res) => {
-  res.json(db.seats);
-});
 
-router.route('/seats').get((req, res) => {
-  res.json(db.seats);
-});
-  
-  router.route('/seats/:id').get((req, res) => {
-  res.json(db.seats.find(i => (i.id === parseInt(req.params.id))));
-});
-  
-router.route('/seats').post((req, res) => {
-  const id = uuidv4();
-  const { day, seat, client, email } = req.body;
+router.get('/seats', SeatController.getAll);
 
-  if(db.seats.some(item => item.seat === seat && item.day === day)){
-    res.status(404).json({ message: "The slot is already taken..." });
-  } 
-  else{
-    db.seats.push({id: id, day: day, seat: seat, client: client, email: email});
-    res.json({message: 'OK'});
+router.get('/seats/:id', SeatController.getById);
 
-    req.io.emit('seatsUpdated', db.seats);
-  }
-});
-  
-router.route('/seats/:id').delete((req, res) => {
-  const item = db.seats.find(i => (i.id === parseInt(req.params.id)));
-  const itemIndex = db.seats.indexOf(item);
-  
-  db.seats.splice(itemIndex, 1);
-  res.json({message: 'OK'});
-});
-  
-router.route('/seats/:id').put((req, res) => {
-  const item = db.seats.find(i => (i.id === parseInt(req.params.id)));
-  const { day, seat, client, email } = req.body;
-  
-  item.day = day;
-  item.seat = seat;
-  item.client = client;
-  item.email = email;
-  
-  res.json({message: 'OK'});
-});
+router.post('/seats', SeatController.post);
+
+router.delete('/seats/:id', SeatController.delete);
+
+router.put('/seats/:id', SeatController.put);
 
 module.exports = router;
